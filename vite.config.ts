518f5b731cd35cd4511eea9e1aec9f5e626ff944
Simone Rizza <simonerizza@macbookpro.home>
2025-01-25 02:28:51 +0100

    fix(deploy): router

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,31 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(async () => {
+  const { viteStaticCopy } = await import('vite-plugin-static-copy')
+
+  return {
+    base: '/event-invitation',
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+    plugins: [
+      vue(),
+      vueDevTools(),
+      viteStaticCopy({
+        targets: [
+          {
+            src: 'dist/index.html',
+            dest: '.',
+            rename: '404.html',
+          },
+        ],
+      }),
+    ],
+  }
 })
